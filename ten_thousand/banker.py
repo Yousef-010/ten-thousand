5c7fb623e35264b41eeac13e7ba58b5ae09d1e6f
Hamad Dwairi <hamaddwairi0@gmail.com>
2022-06-13 12:07:07 +0300

    Bank method & Clear Shelf mehtod

--- ten_thousand/banker.py
+++ ten_thousand/banker.py
@@ -6,8 +6,17 @@ class Banker:
 
     ###################
     # Bank method -  Hamad
+    def bank(self):
+        self.balance = 0
+        self.balance += self.shelved
+        self.shelved = 0
+        return self.balance
     ###################
 
     ###################
     # Clear Shelf method - Hamad
+    def clear_shelf(self):
+        self.shelved = 0
+        return self.shelved
+
     ###################
