efe8ce3bb61d21a0d4d0ff0c8a800597882d18b0
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-06-15 08:41:38 +0300

    fixing paths & create Game class

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,2 +1,13 @@
+import sys
+from ten_thousand.game_logic import GameLogic
+from ten_thousand.banker import Banker
+
+
 class Game:
-    pass
+    def __init__(self):
+        self.bank = Banker()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
--- tests/version_2/test_text_sim.py
+++ tests/version_2/test_text_sim.py
@@ -2,30 +2,30 @@ import pytest
 from tests.flo import diff
 from ten_thousand.game import Game
 
-pytestmark = [pytest.mark.version_2]
+# pytestmark = [pytest.mark.version_2]
 
 
 def test_quitter():
     game = Game()
-    diffs = diff(game.play, path="tests/version_2/quitter.sim.txt")
+    diffs = diff(game.play, path="quitter.sim")
     assert not diffs, diffs
 
 
 def test_one_and_done():
     game = Game()
-    diffs = diff(game.play, path="tests/version_2/one_and_done.sim.txt")
+    diffs = diff(game.play, path="one_and_done.sim")
     assert not diffs, diffs
 
 
 def test_single_bank():
     game = Game()
-    diffs = diff(game.play, path="tests/version_2/bank_one_roll_then_quit.sim.txt")
+    diffs = diff(game.play, path="bank_one_roll_then_quit.sim")
     assert not diffs, diffs
 
 
 def test_bank_first_for_two_rounds():
     game = Game()
-    diffs = diff(game.play, path="tests/version_2/bank_first_for_two_rounds.sim.txt")
+    diffs = diff(game.play, path="bank_first_for_two_rounds.sim")
     assert not diffs, diffs
 
 
@@ -33,12 +33,13 @@ def test_hot_dice():
     """When all dice are used without a zilch
     then user gets 6 fresh dice and continues turn.
     """
-    diffs = diff(Game().play, path="tests/version_2/hot_dice.sim.txt")
+    diffs = diff(Game().play, path="hot_dice.sim")
     assert not diffs, diffs
 
 
 def test_repeat_roller():
     """Allow setting aside scoring dice and rolling the rest
     """
-    diffs = diff(Game().play, path="tests/version_2/repeat_roller.sim.txt")
+    diffs = diff(Game().play, path="repeat_roller.sim")
     assert not diffs, diffs
+
