fe8b3f23bc88de5d12565c9c057a2bfb0187aef3
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-06-18 15:23:55 +0300

    ADD: check cheaters

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,4 +1,5 @@
 
+
 try:
     from game_logic import GameLogic
     from banker import Banker
@@ -15,6 +16,8 @@ class Game:
         self.round = 1
         self.dice = 6
         self.flag = False
+        self.flag_ch = True
+        self.dices_shelved = 0
         self.banker = Banker()
 
     def play(self, roller=GameLogic.roll_dice):
@@ -22,15 +25,18 @@ class Game:
         self.welcome()
         while self.flag:
             print(f'Rolling {self.dice} dice...')
-            self.formatted_roll()
+            print_roll = self.formatted_roll()
             print('Enter dice to keep, or (q)uit:')
             user_answer = input('> ')
             if user_answer == 'q':
                 self.quit()
             if user_answer != 'q':
-                score, dices_shelved = self.roll_and_get_score(user_answer)
-                self.banker.shelf(score)
-                self.dice -= dices_shelved
+                self.check_cheaters(print_roll,  user_answer)
+                if self.flag_ch:
+                    score = self.roll_and_get_score(user_answer)[0]
+                    self.banker.shelf(score)
+                self.flag_ch = True
+                self.dice -= self.dices_shelved
 
                 print(f'You have {self.banker.shelved} unbanked points and {self.dice} dice remaining')
                 print('(r)oll again, (b)ank your points or (q)uit:')
@@ -50,24 +56,59 @@ class Game:
     def formatted_roll(self):
         roller_input = ' '.join(map(str, (self.roller(self.dice))))
         print(f'*** {roller_input} ***')
+        self.check_zilch(roller_input)
+        return roller_input
 
-    @staticmethod
-    def roll_and_get_score(user_answer):
+    def check_zilch(self, roller_input):
+        value = (tuple(map(int, ''.join(roller_input.split(' ')))))
+        if GameLogic.calculate_score(value) == 0:
+            self.zilch()
+        return value
+
+    def roll_and_get_score(self, user_answer):
         dices = tuple([int(x) for x in user_answer])
-        dices_shelved = len(dices)
+        self.dices_shelved = len(dices)
         score = GameLogic.calculate_score(dices)
-        return score, dices_shelved
+        return score,  dices
 
     def zilch(self):
+        self.banker.clear_shelf()
         print('****************************************')
         print('**        Zilch!!! Round over         **')
         print('****************************************')
         print(f"You banked {self.banker.balance} points in round {self.round}")
         print(f"Total score is {self.banker.balance} points")
-        self.banker.clear_shelf()
         self.dice = 6
         self.round += 1
         print(f'Starting round {self.round}')
+        print(f'Rolling {self.dice} dice...')
+        self.formatted_roll()
+
+    @staticmethod
+    def die(user_answer):
+        die = tuple([int(x) for x in user_answer])
+        return die
+
+    def check_cheaters(self, roller_input, user_answer):
+        value = (tuple(map(int, ''.join(roller_input.split(' ')))))
+        dices = self.die(user_answer)
+        # common_ele = set(value) & set(dices)
+        common_ele = []
+        for element1 in value:
+            if element1 in dices:
+                common_ele.append(element1)
+
+        if len(tuple(common_ele)) < len(dices):
+            self.flag_ch = False
+            print('Cheater!!! Or possibly made a typo...')
+            self.dice = 6
+            value = ' '.join(map(str, value))
+            print(f'*** {value} ***')
+            print('Enter dice to keep, or (q)uit:')
+            user_answer = input('> ')
+            score = self.roll_and_get_score(user_answer)[0]
+            self.banker.clear_shelf()
+            self.banker.shelf(score)
 
     def welcome(self):
         print('Welcome to Ten Thousand')
--- tests/version_3/test_sim_2.py
+++ tests/version_3/test_sim_2.py
@@ -4,12 +4,12 @@ from ten_thousand.game import Game
 
 # pytestmark = [pytest.mark.version_3]
 
-#
-# def test_cheat_and_fix():
-#     """Cheating (or typos) should not be allowed.Therefor the user's input must be validated If invalid prompt user for re-entry
-#     """
-#     diffs = diff(Game().play, path="cheat_and_fix.sim")
-#     assert not diffs, diffs
+
+def test_cheat_and_fix():
+    """Cheating (or typos) should not be allowed.Therefor the user's input must be validated If invalid prompt user for re-entry
+    """
+    diffs = diff(Game().play, path="tests/version_3/cheat_and_fix.sim")
+    assert not diffs, diffs
 
 
 def test_zilcher():
